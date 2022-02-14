91049e710a12daddbd49eaf766215aaed1a1f198
jasper <s218005@students.aie.edu>
2022-02-14 11:26:47 -0600

    right now i can print things to the console

--- Dictinary/Dictinary/Dictinary.cpp
+++ Dictinary/Dictinary/Dictinary.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "DictionaryT.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    Dictionary<int,int> m_dictionary;
+    m_dictionary.addItem(1, 1);
+    m_dictionary.addItem(2, 2);
+    m_dictionary.addItem(3, 3);
+    m_dictionary.addItem(4, 4);
 }
 
--- Dictinary/Dictinary/DictionaryT.h
+++ Dictinary/Dictinary/DictionaryT.h
@@ -11,12 +11,12 @@ public:
 	void clear();//deletes all items in dictionary
 	bool containsKey(const TKey object) const;//checks to see if an item that has the given key is in the dictionary
 	bool containsValue(const TValue object) const;//checks to see if an item that has the given value is in the dictionary
-	bool  tryGetValue(const TKey key, const TValue& value) const;//creates a new item with the given key and value and adds it to the dictionary
-	void addItem(const TKey& key, const TValue& value);//tries to find the item that matches the given key
+	bool  tryGetValue(const TKey key,  TValue& value) const;//tries to find the item that matches the given key
+	void addItem(const TKey& key, const TValue& value);//creates a new item with the given key and value and adds it to the dictionary
 	bool remove(const TKey key);//removes the item that has the given key
 	bool remove(const TKey key, TValue& value);//removes the item that has the given key and gives back the value of the item that was removed
 	int  getCount() const;//gets the number of items in the dictionary
-	Dictionary<TKey, TValue>& operator=(const Dictionary<TKey, TValue> other)const;
+	const Dictionary<TKey, TValue>& operator=(const Dictionary<TKey, TValue> other)const;
 	TValue operator[](const TKey key);
 private:
 
@@ -26,7 +26,7 @@ private:
 		TKey itemKey;
 		TValue itemValue;
 	};
-	Item* m_items = nullptr;
+	Item* m_items;
 	int m_count;
 };
 
@@ -34,56 +34,73 @@ template<typename TKey, typename TValue>
 inline Dictionary<TKey, TValue>::Dictionary()
 {
 	m_count = 0;
+	m_items = nullptr;
 }
 
 template<typename TKey, typename TValue>
 inline Dictionary<TKey, TValue>::Dictionary(Dictionary<TKey, TValue>& other)
 {
-	Dictionary::Dictionary() = other;
+	m_count = other.m_count;
+	m_items = other.m_items;
 }
 
 template<typename TKey, typename TValue>
 inline Dictionary<TKey, TValue>::~Dictionary()
 {
+	m_items = nullptr;
+	m_count = 0;
 }
 
 template<typename TKey, typename TValue>
 inline void Dictionary<TKey, TValue>::clear()
 {
-	Dictionary().~Dictionary();
-	//remove();
+	//makes i equal to m_ocunt and if i is less than m_count or equal to...
+	
+	for (int i = m_count; i <= getCount(); i++) 
+	{
+		//makes sure if i is 0
+		if (i == 0)
+			break;//break out of the loop
+		//check to see if the i value dos exist and if it is itemValue
+		if (containsValue(i) == m_items->itemValue)
+		{
+			if (true)//if yes
+			{
+				delete m_items;//delete m_items
+				m_count--;
+			}
+		}
+	}
 }
 
 template<typename TKey, typename TValue>
 inline bool Dictionary<TKey, TValue>::containsKey(const TKey object) const
 {
-	TValue key = 0;
-	if (tryGetValue(object, key))
+	//checks if the itms key is the object
+	if (m_items->itemKey = object)
 	{
-		if (object != 0)
-			return true;
+		return true;//if so return true
 	}
-
+	//otherwise false
 	else return false;
 }
 
 template<typename TKey, typename TValue>
 inline bool Dictionary<TKey, TValue>::containsValue(const TValue object) const
 {
-	TKey key = 0;
-	if (tryGetValue(key, object))
+	//checks if the item value is the object
+	if (m_items->itemValue = object)
 	{
-		if (object != 0)
-			return true;
+		return true;//if so return true
 	}
-
+	//otherwise false
 	else return false;
 }
 
 template<typename TKey, typename TValue>
-inline bool Dictionary<TKey, TValue>::tryGetValue(const TKey key, const TValue& value) const
+inline bool Dictionary<TKey, TValue>::tryGetValue(const TKey key, TValue& value) const
 {
-	//checks if key is not 0
+	//checks if key is not 0 and if the key is greater than the count
 	if (key != 0 || key > m_count) {
 		//checks if this (this is key) contains a value
 		if (this->containsValue(value)) 
@@ -99,35 +116,73 @@ inline bool Dictionary<TKey, TValue>::tryGetValue(const TKey key, const TValue& 
 template<typename TKey, typename TValue>
 inline void Dictionary<TKey, TValue>::addItem(const TKey& key, const TValue& value)
 {
+	Item* m_newItem = new Item();
+	m_newItem->itemKey = key;
+	m_newItem->itemValue = value;
+
+	m_count++;
+	m_items = m_newItem;
+	std::cout << " "<< m_items->itemValue;
+	
 }
 
 template<typename TKey, typename TValue>
 inline bool Dictionary<TKey, TValue>::remove(const TKey key)
 {
-	return false;
+	//if it is not the key return false
+	if (!key) return key;
+
+	//check it the items key is the key that is given
+	if (m_items->itemKey = key)
+	{
+		//this is a pointer to the key
+		//probebly will not work
+		delete &key;
+		m_count--;//decrement the count
+		return true;//return the value
+	}
+	else return false;
 }
 
 template<typename TKey, typename TValue>
 inline bool Dictionary<TKey, TValue>::remove(const TKey key, TValue& value)
 {
-	return false;
+	//if it is not the key return false
+	if (!key) return false;
+
+	//check it the items key is the key that is given
+	if (m_items->itemKey = key)
+	{
+		//checks if the items value is the value
+		if (m_items->itemValue = value)
+		{
+			//this is a pointer to the key
+			//probebly will not work
+			delete &key;
+			m_count--;//decrement the count
+			return value;//return the value
+		}
+		return false;
+	}
+	else return false;
 }
 
 template<typename TKey, typename TValue>
 inline int Dictionary<TKey, TValue>::getCount() const
 {
-	return m_count;
+	if (m_count != NULL) return m_count;
+	else return NULL;
 }
 
 template<typename TKey, typename TValue>
-inline Dictionary<TKey, TValue>& Dictionary<TKey, TValue>::operator=(const Dictionary<TKey, TValue> other) const
+inline const Dictionary<TKey, TValue>& Dictionary<TKey, TValue>::operator=(const Dictionary<TKey, TValue> other) const
 {
-	// TODO: insert return statement here
-	m_items = other.m_items;
+	//
+	*m_items = *other.m_items;
 }
 
 template<typename TKey, typename TValue>
 inline TValue Dictionary<TKey, TValue>::operator[](const TKey key)
 {
-	return TValue();
+	return TValue(key);
 }
