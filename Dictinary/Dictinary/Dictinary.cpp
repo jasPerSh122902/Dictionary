78ac2e06b4a931f6ce4692669a88735c53bc5e27
jasper <s218005@students.aie.edu>
2022-02-14 16:53:21 -0600

    the contains value and key works...
    The add funciton workds but it only add the first index

--- Dictinary/Dictinary/Dictinary.cpp
+++ Dictinary/Dictinary/Dictinary.cpp
@@ -3,11 +3,10 @@
 
 int main()
 {
-    
     Dictionary<int,int> m_dictionary;
-    m_dictionary.addItem(1, 1);
-    m_dictionary.addItem(2, 2);
-    m_dictionary.addItem(3, 3);
-    m_dictionary.addItem(4, 4);
+    m_dictionary.addItem(1, 3);
+    m_dictionary.addItem(2, 563);
+    m_dictionary.addItem(3, 123);
+    m_dictionary.addItem(4, 400);
 }
 
--- Dictinary/Dictinary/DictionaryT.h
+++ Dictinary/Dictinary/DictionaryT.h
@@ -16,6 +16,7 @@ public:
 	bool remove(const TKey key);//removes the item that has the given key
 	bool remove(const TKey key, TValue& value);//removes the item that has the given key and gives back the value of the item that was removed
 	int  getCount() const;//gets the number of items in the dictionary
+
 	const Dictionary<TKey, TValue>& operator=(const Dictionary<TKey, TValue> other)const;
 	TValue operator[](const TKey key);
 private:
@@ -54,130 +55,151 @@ inline Dictionary<TKey, TValue>::~Dictionary()
 template<typename TKey, typename TValue>
 inline void Dictionary<TKey, TValue>::clear()
 {
-	//makes i equal to m_ocunt and if i is less than m_count or equal to...
-	
-	for (int i = m_count; i <= getCount(); i++) 
-	{
-		//makes sure if i is 0
-		if (i == 0)
-			break;//break out of the loop
-		//check to see if the i value dos exist and if it is itemValue
-		if (containsValue(i) == m_items->itemValue)
-		{
-			if (true)//if yes
-			{
-				delete m_items;//delete m_items
-				m_count--;
-			}
-		}
-	}
+	 delete[] m_items;
 }
 
 template<typename TKey, typename TValue>
 inline bool Dictionary<TKey, TValue>::containsKey(const TKey object) const
 {
 	//checks if the itms key is the object
-	if (m_items->itemKey = object)
+	for (int i = 0; i < getCount(); i++) 
 	{
-		return true;//if so return true
+		if (m_items[i].itemKey == object)
+			return true;
 	}
-	//otherwise false
-	else return false;
+	return false;
 }
 
 template<typename TKey, typename TValue>
 inline bool Dictionary<TKey, TValue>::containsValue(const TValue object) const
 {
-	//checks if the item value is the object
-	if (m_items->itemValue = object)
+	//iterate through the arry 
+	for (int i = 0; i < getCount(); i++) 
 	{
-		return true;//if so return true
+		//check if the value of item is equal to object
+		if(m_items[i].itemValue == object)
+			return true;//return true
 	}
-	//otherwise false
-	else return false;
+	return false;
+	
+	
 }
 
 template<typename TKey, typename TValue>
 inline bool Dictionary<TKey, TValue>::tryGetValue(const TKey key, TValue& value) const
 {
-	//checks if key is not 0 and if the key is greater than the count
-	if (key != 0 || key > m_count) {
-		//checks if this (this is key) contains a value
-		if (this->containsValue(value)) 
-		{
-			//return that value
-			return value;
-		}
+	//iterates through the array of items.
+	for (int i = 0; i < getCount(); i++) 
+	{
+		//compares the key and the values
+		if (m_items[i].itemKey == key && m_items[i].itemValue == value)
+			return true; // returns true
 	}
-	//if not
-	else return false;
+	return false;
+	////iterates through the array of items.
+	//for (int i = 0; i < getCount(); i++)
+	//{//iterates through the array of items.
+	//	for (int j = 0; j < getCount(); j++)
+	//	{
+	//		//compares the key and the values
+	//		if (m_items[i].itemKey == key && m_items[j].itemValue == value)
+	//			return true; // returns true
+	//	}
+	//	return false;
+	//}
+	//return false;
 }
 
 template<typename TKey, typename TValue>
 inline void Dictionary<TKey, TValue>::addItem(const TKey& key, const TValue& value)
 {
-	Item* m_newItem = new Item();
-	m_newItem->itemKey = key;
-	m_newItem->itemValue = value;
-
-	m_count++;
-	m_items = m_newItem;
-	std::cout << " "<< m_items->itemValue;
+	//Make new item array that is 1 more
+	Item* newArrayItem = new Item[m_count + 1];
+	//for loop to copy the values in the old to the new
 	
+	for (int i = 0; i < getCount(); i++)
+	{
+		//set the new array at teh index of i to the m_items at the index of i
+		newArrayItem[i] = m_items[i];
+		
+	}
+	//delete old array
+	delete[] m_items;
+	//add the value to the new array
+	newArrayItem[m_count].itemValue = value;
+	newArrayItem[m_count].itemKey = key;
+	
+	//increase count
+	m_count++;
+	//then make m_ites equal to the new array
+	m_items = newArrayItem;
+	
+	//makes the items print
+	std::cout << " " << m_items->itemValue;
 }
 
 template<typename TKey, typename TValue>
 inline bool Dictionary<TKey, TValue>::remove(const TKey key)
 {
-	//if it is not the key return false
-	if (!key) return key;
-
-	//check it the items key is the key that is given
-	if (m_items->itemKey = key)
+	//Make new item array that is 1 less
+	Item* newArrayItem = new Item[m_count - 1];
+	//check if the key exists
+	if (containsKey(key)) 
 	{
-		//this is a pointer to the key
-		//probebly will not work
-		delete &key;
-		m_count--;//decrement the count
-		return true;//return the value
+		//iterate through the array of items...
+		for (int i = 0; i < getCount(); i++)
+		{
+			//iterate with i
+			newArrayItem[i] = m_items[i];
+		}
+		//find its index
+		newArrayItem[m_count].itemKey = key;
+		//delete key
+		delete[] m_items;
+		//copy all of the values form the new array to the old array
+		m_items = newArrayItem;
+		//count - 1
+		m_count--;
 	}
-	else return false;
+	return false;
 }
 
 template<typename TKey, typename TValue>
 inline bool Dictionary<TKey, TValue>::remove(const TKey key, TValue& value)
 {
-	//if it is not the key return false
-	if (!key) return false;
-
-	//check it the items key is the key that is given
-	if (m_items->itemKey = key)
+	//Make a temp item
+	Item* tempItems = new Item[m_count - 1];
+	//chekc if it was a key and value
+	if (containsKey(key) && containsValue(value)) 
 	{
-		//checks if the items value is the value
-		if (m_items->itemValue = value)
+		//iterate through the array of m_items
+		for (int i = 0; i < getCount(); i++) 
 		{
-			//this is a pointer to the key
-			//probebly will not work
-			delete &key;
-			m_count--;//decrement the count
-			return value;//return the value
+			tempItems[i] = m_items[i];
 		}
-		return false;
+		//set the temp items key and value
+		tempItems[m_count].itemKey = key;
+		tempItems[m_count].itemValue = value;
+		//delete[] m_items
+		delete[] m_items;
+		//m_items is temp items
+		m_items = tempItems;
+		m_count--;//decrease m_count
+		
 	}
-	else return false;
+	return false;
 }
 
 template<typename TKey, typename TValue>
 inline int Dictionary<TKey, TValue>::getCount() const
 {
-	if (m_count != NULL) return m_count;
-	else return NULL;
+	return m_count;
 }
 
 template<typename TKey, typename TValue>
 inline const Dictionary<TKey, TValue>& Dictionary<TKey, TValue>::operator=(const Dictionary<TKey, TValue> other) const
 {
-	//
+	
 	*m_items = *other.m_items;
 }
 
